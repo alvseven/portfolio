71d8b758fbe261bcf0e4da27ea4db303d343c183
alvseven <alv7contato@outlook.com>
2024-05-30 15:36:00 -0300

    add underline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen w-2/4 flex-col items-center justify-center m-auto">
       <section className='flex flex-col gap-32 items-center justify-center p-8'>
         <h1 className='text-2xl lg:text-4xl text-pretty'>Seja bem vindo, em breve migrarei todos os meus artigos e conteúdos pra cá</h1>
-        <Link className="lg:text-3xl text-xl" href="/articles">Artigos e threads</Link>
+        <Link className="lg:text-3xl text-xl underline underline-offset-8" href="/articles">Artigos e threads</Link>
       </section>
     </main>
   )
