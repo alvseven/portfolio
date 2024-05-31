5461a1f63c6176a1ea86dca4aac3d6fad06df52e
alvseven <alv7contato@outlook.com>
2024-05-31 10:01:55 -0300

    update font size

--- src/app/articles/(components)/article.tsx
+++ src/app/articles/(components)/article.tsx
@@ -9,7 +9,7 @@ type ArticleProps = {
 
 export function Article({ title, url }: ArticleProps) {
     return (
-        <div className="flex gap-4 items-center w-full lg:text-2xl text-xl">
+        <div className="flex gap-4 items-center w-full  text-xl">
             <TypescriptIcon width={24} height={24} />
             <Link href={url} target='_blank' className="w-10/12">{title}</Link>
         </div>
