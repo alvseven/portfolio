6ac5adb956fa8775449f4898f4b28b17ae758996
alvseven <alv7contato@outlook.com>
2024-07-25 15:49:31 -0300

    how come we dont had analytics yet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-950`}>{children}</body>
+      <body className={`${inter.className} bg-slate-950`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
