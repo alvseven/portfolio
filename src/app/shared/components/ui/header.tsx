33520e38259afe777d67ca690ed7d27fd2affb53
alvseven <alv7contato@outlook.com>
2024-08-22 16:32:29 -0300

    fix header vertical alignment

--- src/app/shared/components/ui/header.tsx
+++ src/app/shared/components/ui/header.tsx
@@ -12,7 +12,7 @@ export const Header = () => {
   const isBasePath = pathname === "/";
 
   return (
-    <header className="top-0 z-10 sticky flex justify-between items-center border-slate-800 bg-background px-8 lg:px-12 py-5 border-b border-double w-full">
+    <header className="top-0 z-10 sticky flex justify-between items-baseline border-slate-800 bg-background px-8 lg:px-12 py-5 border-b border-double w-full">
       <Link href="/" className="animate-pulse">
         <Logo />
       </Link>
