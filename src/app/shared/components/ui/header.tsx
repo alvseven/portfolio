3629a6e98035184e60753673c803d5d9d236c0e1
alvseven <alv7contato@outlook.com>
2024-08-22 16:27:19 -0300

    add go back button

--- src/app/shared/components/ui/header.tsx
+++ src/app/shared/components/ui/header.tsx
@@ -1,17 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import { Logo } from "../icons/logo";
+import { ArrowGoBackIcon } from "../icons/arrow-go-back";
+import { usePathname, useRouter } from "next/navigation";
 
-export const Header = () => (
-  <header className="top-0 z-10 sticky flex justify-between items-center border-slate-800 bg-background px-8 lg:px-12 py-5 border-b border-double w-full">
-    <Link href="/" className="animate-pulse">
-      <Logo />
-    </Link>
-    <nav>
-      <ul className="flex items-center">
-        {/* <li>Primeiro </li>
-        <li>Segundo </li>
-        <li>Terceiro</li> */}
-      </ul>
-    </nav>
-  </header>
-);
+export const Header = () => {
+  const router = useRouter();
+
+  const pathname = usePathname();
+  const isBasePath = pathname === "/";
+
+  return (
+    <header className="top-0 z-10 sticky flex justify-between items-center border-slate-800 bg-background px-8 lg:px-12 py-5 border-b border-double w-full">
+      <Link href="/" className="animate-pulse">
+        <Logo />
+      </Link>
+      <nav>
+        <ul className="flex items-center">
+          {!isBasePath && (
+            <li>
+              <button
+                onClick={() => {
+                  router.back();
+                }}
+              >
+                <ArrowGoBackIcon className="w-6 h-6" />
+              </button>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+};
