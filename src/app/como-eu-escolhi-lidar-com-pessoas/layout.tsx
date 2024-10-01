71684aad58127dc2d719685aacd02695682cb593
alvseven <alv7contato@outlook.com>
2024-09-30 22:40:11 -0300

    add open graph image and metatags

--- src/app/como-eu-escolhi-lidar-com-pessoas/layout.tsx
+++ src/app/como-eu-escolhi-lidar-com-pessoas/layout.tsx
@@ -3,7 +3,24 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Como eu escolhi lidar com pessoas",
   description:
-    "Nesse breve artigo eu compartilho as regrinhas que sigo para manter uma boa convivência com as pessoas ao meu redor, o que também me ajuda a evitar estresse e manter a paz, faz bem pra mim e pra todos ao meu redor",
+    "Descubra como a presunção de boa intenção e a comunicação defensiva podem melhorar seus relacionamentos e promover saúde mental.",
+  keywords:
+    "relacionamentos, saúde mental, comunicação, presunção de boa intenção, comunicação defensiva, desenvolvimento pessoal",
+  authors: [{ name: "Alves", url: "https://alvseven.com" }],
+  openGraph: {
+    title: "Como eu escolhi lidar com pessoas",
+    description:
+      "Aprenda a aplicar a presunção de boa intenção e a comunicação defensiva para transformar suas interações e fortalecer suas relações.",
+    url: "https://www.alvseven.com/como-eu-escolhi-lidar-com-pessoas",
+    type: "article",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Como eu escolhi lidar com pessoas",
+    description:
+      "Descubra como a presunção de boa intenção e a comunicação defensiva podem melhorar seus relacionamentos e promover saúde mental.",
+    creator: "@typescripting",
+  },
 };
 
 export default function RootLayout({
